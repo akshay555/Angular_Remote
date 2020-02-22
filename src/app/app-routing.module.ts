c656b8d4af46f8bfe6b93aa26c40d4d6cc5e8c53
Akshay <akshayhiremath55@gmail.com>
2020-02-22 21:56:41 +0530

    https://github.com/akshay555/Angular_Remote.git

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/department' ,pathMatch:'prefix'},
+  { path: 'employee',      component: EmployeeListComponent },
+  { path: "**",      component: PageNotFoundComponent }
+]
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const  routesdata=[DepartmentListComponent,EmployeeListComponent,PageNotFoundComponent]
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule ,routesdata} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    routesdata,
+    PageNotFoundComponent
+  ],
+  imports: [
+    
+    BrowserModule,
+    AppRoutingModule
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
